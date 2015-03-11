49163f48335b891a43be89dd493388e720bced8c
a.nikitin <a.nikitin@qiwi.ru>
2015-03-11 17:15:21 +0300

    small fix 2

--- src/main/java/com/qiwi/mobile/Worker.java
+++ src/main/java/com/qiwi/mobile/Worker.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-import groovy.lang.GroovyClassLoader;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
